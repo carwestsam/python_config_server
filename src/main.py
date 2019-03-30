81d047afab1dee153a9f64826ab0a605e31f2e8d
Cedric Sung <carwestsam@gmail.com>
2019-03-30 01:27:21 -0700

    dynamic generate config route

--- src/main.py
+++ src/main.py
@@ -1,10 +1,52 @@
 from fastapi import FastAPI
 from aiofile import AIOFile
 import ujson as json
+from os.path import isdir, isfile
 import os
 
+MAX_CONFIG_FILE_SIZE = 409600
+
 app = FastAPI()
-base_url = os.environ['CONFIG_URI'].replace('file://', '')
+
+base_url = os.path.abspath(os.environ['CONFIG_URI'].replace('file://', '')) + '/'
+
+with open(os.path.join(base_url + 'config.json'), 'rb') as conf_file:
+    config = ''.join([line.decode('utf-8') for line in conf_file.readlines()])
+    app_conf = json.loads(config)
+    # print('loaded', app_conf)
+
+
+def readConfFuncGenerator(filepath, version):
+    async def __readConfig():
+        async with AIOFile(filepath, 'rb') as file:
+            data = await file.read(MAX_CONFIG_FILE_SIZE)
+            return {'version': version, 'config': json.loads(data)}
+
+    return __readConfig
+
+
+if "services" in app_conf:
+    for service in app_conf['services']:
+        service_conf = app_conf['services'][service]
+        # print('conf service:', service)
+        dir = service
+        if 'dir' in service_conf:
+            dir = service_conf['dir']
+
+        service_dir = os.path.join(base_url + dir)
+        # print('service_dir', service_dir)
+        latest = {}
+        for version in os.listdir(service_dir):
+            version_dir = os.path.join(service_dir, version)
+            if isdir(version_dir):
+                for env_conf_name in os.listdir(version_dir):
+                    env_conf_path = os.path.join(version_dir, env_conf_name)
+                    if isfile(env_conf_path):
+                        env_conf = os.path.splitext(env_conf_name)[0]
+                        route_path = "/config/{}/{}/{}".format(service, version, env_conf)
+                        app.get(route_path)(readConfFuncGenerator(env_conf_path, version))
+
+        # version_list = [dir for dir in os.listdir(service_dir) if isdir(os.path.join(service_dir, dir))]
 
 
 @app.get("/")
--- test/test_load_configuration.py
+++ test/test_load_configuration.py
@@ -3,12 +3,22 @@ import os
 import ujson as ujson
 from starlette.testclient import TestClient
 
+conf_path = os.environ['CONFIG_URI'] = 'file://' + os.getcwd() + '/../sample_config/'
+
 
 class TestLoadConfiguration(unittest.TestCase):
     def test_initial_config_path(self):
-        conf_path = os.environ['CONFIG_URI'] = 'file://' + os.getcwd() + '/../sample_config/'
-        print(conf_path)
         from main import app
         client = TestClient(app)
         data = ujson.loads(client.request('GET', '/config/config.json').content)
         self.assertEqual(data['version'], '20190329')
+
+    def test_should_load_from_config_file_and_create_service_route(self):
+        from main import app
+        client = TestClient(app)
+        response = client.request('GET', '/config/account-service/20190329/development')
+        self.assertEqual(response.status_code, 200)
+        data = ujson.loads(response.content)
+        self.assertEqual(data['config']['name'], 'account_service_app')
+        response = client.request('GET', '/config/account-service/20181002/development')
+        self.assertEqual(response.status_code, 200)
